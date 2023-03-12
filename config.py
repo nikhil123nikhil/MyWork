1c8ea3b7f61971aae5e62aba9b1b4880cfe2b72f
nikhil123nikhil <106946553+nikhil123nikhil@users.noreply.github.com>
2023-03-12 21:32:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-sudo_users = [1984763765]
+sudo_users = [829259422]
 
 iptv_link = "https://gist.githubusercontent.com/adityaraj2003/c8052a548e3bce3f2ef69f8ac5a593f4/raw/050ccc515b1f6d9d00502cfe132eecfd1a589168/iptv.json"
 
-GROUP_TAG = "Conan76"
+GROUP_TAG = "Nikhil"
 
 
 CHANNELS_TEXT = """<b>Here are the List of Channels : </b>
